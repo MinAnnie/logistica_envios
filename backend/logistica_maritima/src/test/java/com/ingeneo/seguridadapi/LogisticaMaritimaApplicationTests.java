cdeb1604b925fdbb32d50c75f6b1920c5caa16e2
nini✨ <angiematiz1808@gmail.com>
2022-10-19 15:19:20 -0500

    archivos iniciales de logistica maritima y frontend

---
+++ backend/logistica_maritima/src/test/java/com/ingeneo/seguridadapi/LogisticaMaritimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingeneo.seguridadapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticaMaritimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
