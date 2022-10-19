28bd9869b48003715bff3f527489efd4253bcaa0
nini✨ <angiematiz1808@gmail.com>
2022-10-19 15:13:55 -0500

    archivos base de logistica terrestre

---
+++ backend/logistica_terrestre/src/test/java/com/ingeneo/seguridadapi/LogisticaTerrestreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingeneo.seguridadapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticaTerrestreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
